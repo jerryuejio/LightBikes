45ee8e6f617e3b4e1d8e50b6913e0fa3905ef488
jerryu <jerryuejio@gmail.com>
2014-04-19 15:12:17 -0700

    First entry

--- entry.js
+++ entry.js
@@ -1,7 +1,10 @@
 Bot.register ("JerryBot", function(game_state, my_state, done) {
     var me = game_state.me;
     var board = game_state.board;
-    var hexes = board.hexes;
+    var hexes = new Array();
+    for (var i = 0; i < board.hexes.length; i ++) {
+	hexes = hexes.concat(board.hexes[i]);
+    }
     var safe_dirs = board.safe_directions(me);
     var move = 1;
     var regions = new Array(6);
@@ -17,12 +20,56 @@ Bot.register ("JerryBot", function(game_state, my_state, done) {
 	var real_neg_y = my_y - neg_y;
 	return (test.x <= real_pos_x && test.x >= real_neg_x && test.y <= real_pos_y && test.y >= real_neg_y);
     };
+    function printable(hexes) {
+	var r = new Array();
+	for (var i = 0; i < hexes.length; i ++) {
+	    var hex = new Array(2);
+	    hex[0] = hexes[i].x;
+	    hex[1] = hexes[i].y;
+	    r.push(hex);
+	}
+	return r;
+    };
     for (var i = 0; i < hexes.length; i++) {
 	var hex = hexes[i];
-	if (in_range(hex, 3, 1, 3, 5)) {
-	    alert("added");
+	if (in_range(hex, 7, 1, 7, 13)) {
+	    regions[1].push(hex);
+	}
+	if (in_range(hex,1, 1, 13, 13)) {
 	    regions[0].push(hex);
+	} 
+	if (in_range(hex, 13, 1, 1, 13)) {
+	    regions[2].push(hex);
+	}
+	if (in_range(hex, 13, 13, 1, 1)) {
+	    regions[3].push(hex);
+	}
+	if (in_range(hex, 7, 13, 7, 1)) {
+	    regions[4].push(hex);
+	}
+	if (in_range(hex, 1, 13, 13, 1)) {
+	    regions[5].push(hex);
 	}
     }
-    done(move);
+    var best_score = 0;
+    var best_region = 0;
+    for (var i = 0; i < safe_dirs.length; i ++) { 
+	var score = 0;
+	var region_num = safe_dirs[i];
+	var region = regions[region_num];
+	for (var j = 0; j < region.length; j ++) {
+	    var hex = region[j];
+	    if (hex.player === null) {
+		score += 1;
+		if (Math.abs(my_x - hex.x) < 2 || Math.abs(my_y - hex.y) < 2) {
+		    score += 5;
+		}
+	    }
+	}
+	if (score > best_score) {
+	    best_score = score;
+	    best_region = region_num;
+	}
+    }
+    done(best_region);
 })
